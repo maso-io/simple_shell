3e9b704801d767f1ad2eb1d3acf0376b5cd44b7b
Maso <nkombisa.masonwabe@gmail.com>
2023-12-04 13:06:24 +0200

    shell prototype/shell

---
+++ prototype/shell_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>/* import exit */
+#include <unistd.h>/* */
+#include <sys/wait.h>/* import wait */
+#include <sys/type.h>/* import pid_t */
+/**
+ * main - creates a simple shell
+ *
+ * Return: always 0.
+ */
+int main()
+{
+	/* 1. get the line */
+
+	/* 2. chop it up to pieces  so to have the different args */
+
+	/* 3. create a child process to execute of it */
+
+	/* execute */
+
+	return (0);
+}
