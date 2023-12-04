0f9f15cfb52c77ef0ebd8a7e45864b6a481ebec5
masonwabe nkombisa <nkombisa.masonwabe@gmail.com>
2023-12-04 13:09:39 +0000

    test files

---
+++ prototype/shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL
+#define SHELL
+
+#include <stdio.h> /* EOF macro */
+#include <stdlib.h>/* import exit */
+#include <unistd.h>/* */
+#include <sys/wait.h>/* import wait */
+#include <sys/types.h>/* import pid_t */
+#include <string.h> /* import string utilities */
+
+/**
+ * ul_int - Unsigned long int type definition
+ */
+typedef unsigned long int ul_int;
+
+#endif /* shell.h */
--- prototype/shell_1.c
+++ prototype/shell_1.c
@@ -1,8 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>/* import exit */
-#include <unistd.h>/* */
-#include <sys/wait.h>/* import wait */
-#include <sys/type.h>/* import pid_t */
+#include "shell.h"
+/**
+* _getline - gets input from the user
+* @buffer: buffer for user input
+*
+* Return: Number of characters printed or EOF on error
+*/
+int _getline(char **buffer);
+
 /**
  * main - creates a simple shell
  *
@@ -10,8 +14,20 @@
  */
 int main()
 {
-	/* 1. get the line */
+	char *buffer, **arr;
 
+	buffer = (char *)malloc(100);
+	if (!buffer)
+		return (-1);
+	/* 1. get the line */
+	printf("$ ");
+	while(_getline(&buffer) != EOF)
+	{
+		//arr = split(&lineptr);
+		printf("---- TEST: BUFFER --> %s <----\n", *buffer);
+		printf("$ ");
+	}
+	putchar(10);
 	/* 2. chop it up to pieces  so to have the different args */
 
 	/* 3. create a child process to execute of it */
@@ -19,4 +35,25 @@ int main()
 	/* execute */
 
 	return (0);
+}
+int _getline(char **buffer)
+{
+	int len;
+	size_t n;
+	char *str, *lineptr = NULL;
+
+	n = 0;
+	if (getline(&lineptr, &n, stdin) == EOF)
+	{
+		free(lineptr);
+		return (EOF);
+	}
+	len = strlen(lineptr);
+	buffer = (char *)realloc(sizeof(char) * strlen(buffer));
+	*buffer = strdup(lineptr);
+	*buffer[len - 1] = '\0';
+
+	printf("%s\n", *buffer);
+
+	return (len - 1);
 }
